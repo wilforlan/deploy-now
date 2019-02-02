50b27fabb958c57ec7ca8d82aef48be7a8999f58
Williams Isaac <williamscalg@gmail.com>
2019-02-02 05:15:08 +0100

    moved method calls to runner file

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 #!/usr/bin/env node
 
-const Loader = require("./lib/loader");
-const Zipper = require("./lib/zipper");
-const Shell = require("./lib/shell");
-(async () => {
-    Loader.Init();
-    try {
-        let config = await Loader.Config();
-        var zipped = await Zipper(config).Archive();
-        config = { ...config, zipped }
-        Shell(config).Start();
-    } catch (error) {
-        console.error("[DEPLOY-NOW] ERROR: "+ error)
-    }
-})();
+const Runner = require("./lib/runner");
+Runner(process.argv).Start();
